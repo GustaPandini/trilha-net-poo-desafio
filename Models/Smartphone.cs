a3175c47711ef23f4bd94bcc34752517374b4d3d
GustaPandini <gugui.pandini@gmail.com>
2025-07-17 19:53:29 -0300

    Classe Smartphone completada

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,14 +1,22 @@
+using System.Dynamic;
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        // Implementado as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
+        private string Imei { get; set; }
+        private int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+            // Passado os parâmetros do construtor para as propriedades
         }
 
         public void Ligar()
